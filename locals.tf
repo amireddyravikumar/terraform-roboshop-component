8c99f5e0ed24a3edfc6756edac300dd2be78f713
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-09-05 18:05:24 +0530

    roboshop dev infra

--- locals.tf
+++ locals.tf
@@ -6,7 +6,7 @@ locals {
   frontend_alb_listener_arn =  data.aws_ssm_parameter.frontend_alb_listener_arn.value
   alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  host_header = var.component == "frontend" ? "${var.project}-${var.environment}.${var.domain_name}" : "${var.component}-backend-alb-${var.environment}.${var.domain_name}" 
+  host_header = var.component == "frontend" ? "${var.project}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}" 
   common_name = "${var.project}-${var.environment}-${var.component}"
   common_tags = {
     Project = "${var.project}"
